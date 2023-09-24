d150e94a53fcf0fa5fe392dad8ea149060d75c79
Kevin Tu <kevintu002@gmail.com>
2023-09-24 17:56:11 -0400

    +

--- config.py
+++ config.py
@@ -61,8 +61,8 @@ config = {
         {
             "index": 2,
             "class": "paladin", # paladin
-            "ilvl-endless": 1540,
-            "ilvl-aor": 1540,
+            "ilvl-endless": 1580,
+            "ilvl-aor": 1580,
             "lopang": False,
             "guildDonation": False,
             "rapport": False,
