a43f1df3818b86036f9909046adffc410f18d499
Kevin <kevintu002@gmail.com>
2023-10-16 00:55:38 -0400

    slayer ilvl

--- config.py
+++ config.py
@@ -97,8 +97,8 @@ config = {
         {
             "index": 6,
             "class": "slayer", # slayer
-            "ilvl-endless": 1580,
-            "ilvl-aor": 1580,
+            "ilvl-endless": 1600,
+            "ilvl-aor": 1600,
             "lopang": False,
             "guildDonation": False,
             "rapport": False,
